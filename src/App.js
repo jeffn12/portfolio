778ac0932992e107d21062bc6c532c5fc91481b4
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-10 18:55:35 -0400

    fix: add bottom margin to navbar

--- src/App.js
+++ src/App.js
@@ -8,18 +8,16 @@ function App() {
 		<>
 			<Navbar />
 			<Container>
-				{
-					<Box my={2}>
-						{[...new Array(12)]
-							.map(
-								() => `Cras mattis consectetur purus sit amet fermentum.
+				<Box>
+					{[...new Array(12)]
+						.map(
+							() => `Cras mattis consectetur purus sit amet fermentum.
 Cras justo odio, dapibus ac facilisis in, egestas eget quam.
 Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
 Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-							)
-							.join("\n")}
-					</Box>
-				}
+						)
+						.join("\n")}
+				</Box>
 			</Container>
 		</>
 	);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,77 +1,78 @@
 import React from "react";
 // Material UI
 import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Avatar,
-  useScrollTrigger,
-  CssBaseline,
+	AppBar,
+	Toolbar,
+	IconButton,
+	Typography,
+	Avatar,
+	useScrollTrigger,
+	CssBaseline
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import MenuIcon from "@material-ui/icons/Menu";
 
 function Navbar() {
-  const Styles = useStyles();
+	const Styles = useStyles();
 
-  return (
-    <div className={Styles.root}>
-      <CssBaseline />
-      <ElevationScroll>
-        <AppBar>
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              className={Styles.menuButton}
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={Styles.title}>
-              Jeff Neuberger
-            </Typography>
-            <Avatar
-              alt="headshot photo of Jeff Neuberger"
-              src="/jeff_neuberger.jpeg"
-              className={Styles.avatar}
-            />
-          </Toolbar>
-        </AppBar>
-      </ElevationScroll>
-    </div>
-  );
+	return (
+		<div className={Styles.root}>
+			<CssBaseline />
+			<ElevationScroll>
+				<AppBar>
+					<Toolbar>
+						<IconButton
+							edge="start"
+							color="inherit"
+							className={Styles.menuButton}
+							aria-label="menu"
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography variant="h6" className={Styles.title}>
+							Jeff Neuberger
+						</Typography>
+						<Avatar
+							alt="headshot photo of Jeff Neuberger"
+							src="/jeff_neuberger.jpeg"
+							className={Styles.avatar}
+						/>
+					</Toolbar>
+				</AppBar>
+			</ElevationScroll>
+		</div>
+	);
 }
 
 export default Navbar;
 
 const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
-    position: "sticky",
-  },
-  menuButton: {
-    marginRight: "2rem",
-  },
-  title: {
-    flexGrow: 1,
-  },
-  avatar: {
-    width: "3rem",
-    height: "3rem",
-  },
+	root: {
+		flexGrow: 1,
+		position: "sticky",
+		marginBottom: "4.5rem"
+	},
+	menuButton: {
+		marginRight: "2rem"
+	},
+	title: {
+		flexGrow: 1
+	},
+	avatar: {
+		width: "3rem",
+		height: "3rem"
+	}
 });
 
 const ElevationScroll = (props) => {
-  const { children } = props;
+	const { children } = props;
 
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-  });
+	const trigger = useScrollTrigger({
+		disableHysteresis: true,
+		threshold: 0
+	});
 
-  return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
-  });
+	return React.cloneElement(children, {
+		elevation: trigger ? 4 : 0
+	});
 };
