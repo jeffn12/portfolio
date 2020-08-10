e6422917615f3f9920eb2d6d95db73638a98bea6
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-10 18:48:38 -0400

    chore: file formatting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React from "react";
 // Components
-import { Box, Container } from '@material-ui/core'
-import Navbar from './components/Navbar'
+import { Box, Container } from "@material-ui/core";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Container>
-      {<Box my={2}>
-          {[...new Array(12)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
+	return (
+		<>
+			<Navbar />
+			<Container>
+				{
+					<Box my={2}>
+						{[...new Array(12)]
+							.map(
+								() => `Cras mattis consectetur purus sit amet fermentum.
 Cras justo odio, dapibus ac facilisis in, egestas eget quam.
 Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-            )
-            .join('\n')}
-        </Box>}</Container>
-    </>
-  );
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+							)
+							.join("\n")}
+					</Box>
+				}
+			</Container>
+		</>
+	);
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,59 +1,77 @@
-import React from 'react'
+import React from "react";
 // Material UI
-import { AppBar, Toolbar, IconButton, Typography, Avatar, useScrollTrigger, CssBaseline } from '@material-ui/core'
-import { makeStyles } from '@material-ui/styles'
-import MenuIcon from '@material-ui/icons/Menu'
-
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Avatar,
+  useScrollTrigger,
+  CssBaseline,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function Navbar() {
-    const Styles = useStyles();
+  const Styles = useStyles();
 
-    return (
-        <div className={Styles.root}>
-            <CssBaseline />
-        <ElevationScroll>
+  return (
+    <div className={Styles.root}>
+      <CssBaseline />
+      <ElevationScroll>
         <AppBar>
-            <Toolbar>
-                <IconButton edge="start" color="inherit" className={Styles.menuButton} aria-label="menu" >
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" className={Styles.title}>Jeff Neuberger</Typography>
-                <Avatar alt="headshot photo of Jeff Neuberger" src="/jeff_neuberger.jpeg" className={Styles.avatar}/>
-            </Toolbar>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              className={Styles.menuButton}
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={Styles.title}>
+              Jeff Neuberger
+            </Typography>
+            <Avatar
+              alt="headshot photo of Jeff Neuberger"
+              src="/jeff_neuberger.jpeg"
+              className={Styles.avatar}
+            />
+          </Toolbar>
         </AppBar>
-        </ElevationScroll>
-        </div>
-    )
+      </ElevationScroll>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
 
 const useStyles = makeStyles({
-    root: {
-        flexGrow: 1,
-        position: "sticky" 
-    },
-    menuButton: {
-        marginRight: "2rem"
-    },
-    title: {
-        flexGrow: 1
-    },
-    avatar: {
-        width: "3rem",
-        height: "3rem"
-    }
-})
+  root: {
+    flexGrow: 1,
+    position: "sticky",
+  },
+  menuButton: {
+    marginRight: "2rem",
+  },
+  title: {
+    flexGrow: 1,
+  },
+  avatar: {
+    width: "3rem",
+    height: "3rem",
+  },
+});
 
 const ElevationScroll = (props) => {
-    const { children } = props;
-    
-    const trigger = useScrollTrigger({
-      disableHysteresis: true,
-      threshold: 0,
-    });
-  
-    return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
-    });
-  }
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+};
