da4dc406e754796dc49f032dcd3e7f1021d7ad36
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-10 18:10:16 -0400

    feat: add photo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,42 @@
 import React from 'react'
 // Material UI
-import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, IconButton, Typography, Avatar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 import MenuIcon from '@material-ui/icons/Menu'
 
+
 function Navbar() {
+    const Styles = useStyles();
+
     return (
+        <div className={Styles.root}>
         <AppBar>
             <Toolbar>
-                <IconButton edge="start" color="inherit" aria-label="menu" >
+                <IconButton edge="start" color="inherit" className={Styles.menuButton} aria-label="menu" >
                     <MenuIcon />
                 </IconButton>
-                <Typography variant="h6">Jeff Neuberger</Typography>
+                <Typography variant="h6" className={Styles.title}>Jeff Neuberger</Typography>
+                <Avatar alt="headshot photo of Jeff Neuberger" src="/jeff_neuberger.jpeg" className={Styles.avatar}/>
             </Toolbar>
         </AppBar>
+        </div>
     )
 }
 
 export default Navbar
+
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1 
+    },
+    menuButton: {
+        marginRight: "2rem"
+    },
+    title: {
+        flexGrow: 1
+    },
+    avatar: {
+        width: "3rem",
+        height: "3rem"
+    }
+})
