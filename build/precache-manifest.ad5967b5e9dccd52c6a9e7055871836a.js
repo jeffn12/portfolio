58f544f14b76dc6f433a8db2030b6af7e0ce5765
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-12 19:26:58 -0400

    feat: build go live

---
+++ build/precache-manifest.ad5967b5e9dccd52c6a9e7055871836a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae6f0169b4696f6fcb358ae45c79a2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7daedaa60e4cbb22db7",
+    "url": "/static/css/main.3ecc932b.chunk.css"
+  },
+  {
+    "revision": "bc115563152941cb86f4",
+    "url": "/static/js/2.2dd36c66.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.2dd36c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7daedaa60e4cbb22db7",
+    "url": "/static/js/main.c31de691.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
