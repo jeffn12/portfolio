88398d7b1705ffe1eaa30942896a99664c4ea083
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-12 17:40:30 -0400

    feat: update headshot image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 // Components
-import { Box, Container } from "@material-ui/core";
 import Background from "./components/Background";
 import Navbar from "./components/Navbar";
 import SplashPage from "./components/SplashPage";
@@ -9,18 +8,15 @@ import ProjectPage from "./components/ProjectPage";
 import SkillsPage from "./components/SkillsPage";
 import ContactPage from "./components/ContactPage";
 import Footer from "./components/Footer";
+// Material UI
+import { Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
 function App() {
+  const Styles = useStyles();
+
   return (
-    <Box
-      mx={2}
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        minWidth: "375px"
-      }}
-    >
+    <Box className={Styles.root}>
       <Background />
       <Navbar />
       <SplashPage />
@@ -28,23 +24,19 @@ function App() {
       <ProjectPage />
       <SkillsPage />
       <ContactPage />
-      {
-        <Container>
-          <Box>
-            {[...new Array(20)]
-              .map(
-                () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-              )
-              .join("\n")}
-          </Box>
-        </Container>
-      }
       <Footer />
     </Box>
   );
 }
 
 export default App;
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    minWidth: "375px",
+    margin: "0rem 2rem"
+  }
+});
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,25 @@
 import React from "react";
 // Material UI
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Avatar } from "@material-ui/core";
 
 function AboutMe() {
   return (
-    <Box>
-      <Typography>{ABOUT_ME}</Typography>
+    <Box style={{ height: "15rem" }}>
+      <Typography align="right" style={{ fontSize: "1.3rem" }}>
+        {ABOUT_ME}
+      </Typography>
+      <Avatar
+        src="/jeff_neuberger.png"
+        variant="rounded"
+        style={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          width: "10rem",
+          height: "10rem",
+          opacity: 0.8
+        }}
+      />
     </Box>
   );
 }
@@ -14,4 +28,5 @@ export default AboutMe;
 
 const ABOUT_ME = `I am a teacher turned software engineer who feels that controlling your own 
   learning is the key to education. I want to support learners on a large scale 
-  realize their abilities through quality web applications.`;
+  realize their abilities through quality web applications. I am passionate about data, and
+  I truly love any project that involves a database!`;
