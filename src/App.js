46de8f518d150829d739cec9b8ba7494b318713d
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-11 20:30:25 -0400

    feat: add background image

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from "react";
 // Components
 import { Box, Container } from "@material-ui/core";
+import Background from "./components/Background";
 import Navbar from "./components/Navbar";
 
 function App() {
-	return (
-		<>
-			<Navbar />
-			<Container>
-				<Box>
-					{[...new Array(12)]
-						.map(
-							() => `Cras mattis consectetur purus sit amet fermentum.
+  return (
+    <>
+      <Background />
+      <Navbar />
+      <Container>
+        <Box>
+          {[...new Array(12)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
 Cras justo odio, dapibus ac facilisis in, egestas eget quam.
 Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
 Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-						)
-						.join("\n")}
-				</Box>
-			</Container>
-		</>
-	);
+            )
+            .join("\n")}
+        </Box>
+      </Container>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Background.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+
+function Background() {
+  const [opacity, setOpacity] = useState(1);
+  const [filter, setFilter] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => {
+      setOpacity(scrollOpacity());
+      setFilter(scrollFilter());
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return function cleanup() {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
+  return (
+    <React.Fragment>
+      <div
+        style={{
+          display: "block",
+          padding: 0,
+          position: "fixed",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          zIndex: -1
+        }}
+      >
+        <img
+          alt="open double doors"
+          style={{
+            opacity: opacity,
+            filter: `blur(${filter}px) brightness(0.7)`,
+            position: "absolute",
+            background:
+              "url('/public/philipp-berndt-5i0GnoTTjSE-unsplash.jpg')",
+            backgroundSize: "cover",
+            top: "-10%",
+            bottom: "10%",
+            left: "-10%",
+            right: "10%",
+            width: "120%",
+            height: "120%"
+          }}
+        />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Background;
+
+function scrollOpacity() {
+  const winScroll =
+    document.body.scrollTop || document.documentElement.scrollTop;
+
+  const height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+  const scrolled = winScroll / height;
+  const newOpacity = 1 + scrolled * 2;
+  return newOpacity;
+}
+
+function scrollFilter() {
+  const winScroll =
+    document.body.scrollTop || document.documentElement.scrollTop;
+
+  const newFilter = 0 + winScroll / 100;
+  return newFilter;
+}
