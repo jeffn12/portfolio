8a67a84821fd55ffde71482e9565d7129ddae15c
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-13 15:21:46 -0400

    feat: add link to repo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,18 @@
 import React from "react";
 // Material UI
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Link as MUILink } from "@material-ui/core";
 
 function Footer() {
   return (
     <Box
       width={1}
-      style={{ marginTop: "5rem", marginBottom: "2rem", textAlign: "center" }}
+      style={{
+        marginTop: "5rem",
+        marginBottom: "2rem",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+      }}
     >
       <Typography
         align="center"
@@ -15,6 +21,15 @@ function Footer() {
       >
         © 2020 Jeff Neuberger
       </Typography>
+      <MUILink href="https://github.com/jeffn12/jeffn12" target="_blank">
+        <Typography
+          align="center"
+          variant="caption"
+          style={{ fontFamily: "Lora" }}
+        >
+          see the code for this website
+        </Typography>
+      </MUILink>
     </Box>
   );
 }
